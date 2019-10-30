82cf08bd47d8a528ae8dee9ca2663e27746a9714
Andy Shih <a.shih@coldwhere.com>
2019-10-30 13:35:56 -0500

    Updates

---
+++ precache-manifest.50b8592139a8b171ebaf0862cdfceff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ba0126f3061aaef7fd0095151a0d43",
+    "url": "/Gif-Search/index.html"
+  },
+  {
+    "revision": "3d865df2dc253345cf98",
+    "url": "/Gif-Search/static/css/main.ad86e12b.chunk.css"
+  },
+  {
+    "revision": "275d32728846035c449e",
+    "url": "/Gif-Search/static/js/2.249b7db9.chunk.js"
+  },
+  {
+    "revision": "3d865df2dc253345cf98",
+    "url": "/Gif-Search/static/js/main.fa61e26c.chunk.js"
+  },
+  {
+    "revision": "06e2b5756287197611b1",
+    "url": "/Gif-Search/static/js/runtime-main.ecd93ae0.js"
+  }
+]);
